aa44161fc6318f11176fa2526a18725abe67c623
Rebekah Gall <fyrhino@outlook.com>
2020-01-08 18:23:23 -0600

    Added Player Class, declared variables to pass into class

---
+++ RPG Battle/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Battle
+{
+    class Player
+    {
+        private int hp;
+        private string name;
+        private int attack;
+        private int defense;
+        private int agility;
+
+        public void setup(string name, int attack, int defense, int agility)
+        {
+            this.name = name;
+            this.attack = attack;
+            this.defense = defense;
+            this.agility = agility;
+        }
+    }
+}
---
+++ RPG Battle/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace RPG_Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player human = new Player();
+            Player npc = new Player();
+            Random random = new Random();
+
+            string classChoice;
+            string tempName;
+            int tempAttack = 0;
+            int tempDefense = 0;
+            int tempAgility = 0;
+            int npcChoice = random.Next(1, 4);
+            Console.WriteLine("What is your name?");
+            tempName = Console.ReadLine();
+            Console.WriteLine("Would you rather be a Knight (k), Rogue (r), or Tank (t)?");
+            classChoice = Console.ReadLine();
+            if (classChoice == "k")
+            {
+                tempAttack = 10;
+                tempDefense = 5;
+                tempAgility = 5;
+            }
+            else if (classChoice == "r")
+            {
+                tempAttack = 5;
+                tempDefense = 5;
+                tempAgility = 10;
+            }
+            else if (classChoice == "t")
+            {
+                tempAttack = 5;
+                tempDefense = 10;
+                tempAgility = 5;
+            }
+            human.setup(tempName, tempAttack, tempDefense, tempAgility);
+            if (npcChoice == 1)
+            {
+                tempAttack = 10;
+                tempDefense = 5;
+                tempAgility = 5;
+            }
+            else if (npcChoice == 2)
+            {
+                tempAttack = 5;
+                tempDefense = 5;
+                tempAgility = 10;
+            }
+            else if (npcChoice == 3)
+            {
+                tempAttack = 5;
+                tempDefense = 10;
+                tempAgility = 5;
+            }
+            npc.setup(tempName, tempAttack, tempDefense, tempAgility);
+
+        }
+    }
+}
